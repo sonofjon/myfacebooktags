e90b14653c62fe9d25d31eb5df150b4782031499
Andreas Jonsson <andreasjon@gmail.com>
2020-12-17 14:12:03 +0100

    Add plugin script

---
+++ my-facebook-tags.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: My Facebook Tags
+ * Plugin URI: http://
+ * Description: This plugin adds some Facebook Open Graph tags to our single posts.
+ * Version: 1.0.0
+ * Author: Andreas H. Jonsson
+ * Author URI: http://
+ * License: GPL2
+ */
